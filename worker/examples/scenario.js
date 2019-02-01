6558cc219c785a369918e6f2ecf2f89dc0b429e3
Austen Higgins-Cassidy <ahigginscassidy@optirtc.com>
2019-02-01 13:37:55 -0500

    Better examples

--- worker/examples/scenario.js
+++ worker/examples/scenario.js
@@ -2,8 +2,6 @@ const Gpio = require('onoff').Gpio;
 const config = require('config');
 const SerialPort = require('serialport');
 
-
-
 // Promise chain
 // first -> run
 
@@ -16,10 +14,10 @@ class TestScenario {
         this.timeout = null;
         this.root_promise = null;
         this.root_start = null;
-        this.messageWaits = [];
         this.port = null;
         this.pass = null;
         this.fail = null;
+        this.line_callback = null;
     }
 
     first()
@@ -39,16 +37,7 @@ class TestScenario {
             this.port = new SerialPort(config.get('Worker.port'),(e) => (this.onPortError(e)));
             this.port.open();
             const readline = new SerialPort.parsers.Readline({ delimiter: '\r\n' });
-            readline.on('data', (data) => {
-                if (this.messageWaits.length > 0)
-                {
-                    let remove = this.messageWaits[0](data);
-                    if (remove)
-                    {
-                        this.messageWaits.shift();
-                    }
-                }
-            });
+            readline.on('data', (data) => { if (this.line_callback != null) this.line_callback(data); });
             this.port.pipe(readline);
             this.root_start(); // resolves the root promise and starts the chain
         });
@@ -77,71 +66,46 @@ class TestScenario {
         });
     }
 
+    waitForInput(test, err = "waitForInput", timeout = this.DEFAULT_TIMEOUT)
+    {
+        return new Promise((resolve, reject) => {
+            let expire = setTimeout(() => reject("Timeout " + err), timeout);
+            this.line_callback = (data) => {
+                if (test(data))
+                {
+                    clearTimeout(expire);
+                    resolve();
+                }
+            };
+        });
+    }
+
     getMessage(msg)
     {
-        return new Promise((resolve, reject) =>
-        {
-           this.messageWaits.push((input) => {
-                if (input == msg)
-                {
-                    resolve();
-                    return true;
-                }
-                return false;
-           });
-        });
+        return this.waitForInput((data) => data == msg, msg);
     }
 
     getRegex(test)
     {
-        return new Promise((resolve, reject) =>
-        {
-            this.messageWaits.push((msg) => {
-                let match = test.exec(msg);
-                if (match != null)
-                {
-                    resolve(match);
-                    return true;
-                }
-                return false;
-            });
-        });
+        return this.waitForInput((data) => {
+            let match = test.exec(msg);
+            return (match != null);
+        }, "regex: " + test);
     }
 
     getState(expected_state)
     {
-        return new Promise((resolve, reject) =>
-        {
-            this.messageWaits.push((msg) => {
-                let device_state = this.parseDeviceState(msg);
-                for(let key in expected_state)
+        this.waitForInput((data) => {
+            let device_state = this.parseDeviceState(msg);
+            for(let key in expected_state)
+            {
+                if (expected_state[key] != device_state[key])
                 {
-                    if (expected_state[key] != device_state[key])
-                    {
-                        return false;
-                    }    
-                }
-                resolve(device_state);
-                return true;
-            });
-        });
-    }
-
-    waitFor(func)
-    {
-        return new Promise((resolve, reject) =>
-        {
-            this.messageWaits.push((msg) => {
-                var res = func(msg);
-                if (res)
-                {
-                    resolve(res);
-                    return true;
-                } else {
                     return false;
-                }
-            });
-        });
+                }    
+            }
+            return true;
+        }, "get state: " + JSON.stringify(expected_state));
     }
 
     parseDeviceState(message)
@@ -157,10 +121,10 @@ class TestScenario {
 
 const defaultTest = new TestScenario();
 // Build promise chain
-defaultTest.first().then(
-    defaultTest.setPower(true).then(() =>
-    defaultTest.getMessage("Test Starting").then(() =>
-    defaultTest.getState({ propertyname: 0}).then(() =>
-    defaultTest.pass()))));
+defaultTest.first().then(() =>
+defaultTest.setPower(true)).then(() =>
+defaultTest.getMessage("Test Starting")).then(() =>
+defaultTest.getState({ propertyname: 0})).then(() =>
+defaultTest.pass());
 
 exports.default = defaultTest;
